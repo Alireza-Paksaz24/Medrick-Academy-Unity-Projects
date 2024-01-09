848f70733d0018daba09738e633f97b7699ad37b
Alireza Paksasz <a.paksaz@outlook.com>
2024-01-10 02:34:30 +0330

    Manage Spawn Platform base on Player Position

--- Doodle Jump/Assets/Scripts/Player.cs
+++ Doodle Jump/Assets/Scripts/Player.cs
@@ -7,18 +7,28 @@ public class Player : MonoBehaviour
 {
     private Vector2 _speed;
     private Rigidbody2D rb;
+    private float lastPlatformY = -4;
     [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private SpawnManager _spawnManager;
+    [SerializeField] private GameManager _gameManager;
     
     // Start is called before the first frame update
     void Start()
     {
         _speed = new Vector2(0,0);
-        rb.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        lastPlatformY = _spawnManager.SpawnPlatforms(lastPlatformY, _gameManager.level);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (lastPlatformY - this.transform.position.y < 5)
+        {
+            _gameManager.level++;
+            lastPlatformY = _spawnManager.SpawnPlatforms(lastPlatformY, _gameManager.level);
+        }
+
         _speed.y -= _gravity;
         rb.velocity = _speed;
     }
