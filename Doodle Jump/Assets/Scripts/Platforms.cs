3bf974f51ccdfe1d266c71fdb8aa14c3bf2d3bcd
Alireza Paksasz <a.paksaz@outlook.com>
2024-01-10 02:34:50 +0330

    delete off-screen platforms

--- Doodle Jump/Assets/Scripts/Platforms.cs
+++ Doodle Jump/Assets/Scripts/Platforms.cs
@@ -1,18 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Platforms : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _camera;
+    private GameManager _gameManager;
+    [SerializeField]private float _speed = 15.0f;
+    private void Start()
     {
-        
+        _camera = GameObject.FindWithTag("MainCamera");
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_camera.transform.position.y - this.transform.position.y > 4.3)
+            Destroy(this.gameObject);
+        if (this.tag == "Mover_Plat")
+        {
+            this.transform.Translate(Vector3.left * _speed * _gameManager.level / 2);
+            if (this.transform.position.x < -2.0f || this.transform.position.x > 2.0f)
+                _speed *= -1;
+        }
     }
 }
