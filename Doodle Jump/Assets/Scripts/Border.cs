609ca4d01c786d2d1849d2de5d4cd798a64f74e7
Alireza Paksasz <a.paksaz@outlook.com>
2024-01-18 02:40:43 +0330

    Set A border for Destroy Object outside the scene

--- Doodle Jump/Assets/Scripts/Border.cs
+++ Doodle Jump/Assets/Scripts/Border.cs
@@ -7,10 +7,8 @@ using UnityEngine;
 public class Border : MonoBehaviour
 {
     // Start is called before the first frame update
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag.Contains("Plat") && this.name == "Top")
-            return;
         if (!other.gameObject.CompareTag("Player"))
         {
             Destroy(other.gameObject);
