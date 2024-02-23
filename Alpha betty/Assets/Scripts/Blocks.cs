a6d65fea7ff00c5e99d2c98585cdcafb54664cf0
Alireza Paksasz <a.paksaz@outlook.com>
2024-02-23 20:17:34 +0330

    select blocks

--- Alpha betty/Assets/Scripts/Blocks.cs
+++ Alpha betty/Assets/Scripts/Blocks.cs
@@ -1,104 +1,73 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Blocks : MonoBehaviour
+public class Blocks : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler, IPointerUpHandler
 {
-    private string[] words = { "Hello", "World", "Array", "Random", "Words" };
-    private char[,] board = new char[5, 5];
+    private static bool _isClicked = false;
+    private static string word = "";
+    private bool _isSelected = false;
+    private static List<Image> _selected = new List<Image>();
+    private Image blockImageComponent;
+    private char character;
 
-    void Start()
+    private void Start()
     {
-        // Select a random word with less than 25 characters
-        string chosenWord = words[Random.Range(0,words.Length)];
-
-        // Try to place the word
-        if (!PlaceWordInBoard(board, chosenWord))
-        {
-            Debug.Log("Failed to place the word. Trying again...");
-            // Instead of calling Main again, we'll just stop execution here
-            // Consider adjusting your logic to retry within Unity's update loop or through a user action
-            return;
-        }
-
-        // Fill the rest of the board with random letters
-        FillBoard(board);
-
-        // Print the board
-        PrintBoard(board);
+        blockImageComponent = GetComponent<Image>();
+        character = 'a';
     }
-
-    bool PlaceWordInBoard(char[,] board, string word)
+    public void OnPointerDown(PointerEventData eventData)
     {
-        List<(int, int)> possiblePositions = new List<(int, int)>();
-
-        for (int row = 0; row < board.GetLength(0); row++)
+        if (eventData.button == PointerEventData.InputButton.Left)
         {
-            for (int col = 0; col < board.GetLength(1); col++)
-            {
-                possiblePositions.Add((row, col));
-            }
-        }
-
-        while (possiblePositions.Count > 0)
-        {
-            int posIndex = Random.Range(0,possiblePositions.Count);
-            (int row, int col) = possiblePositions[posIndex];
-            possiblePositions.RemoveAt(posIndex);
-
-            if (PlaceWordFromPosition(board, word, 0, row, col, new bool[5, 5]))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    bool PlaceWordFromPosition(char[,] board, string word, int index, int row, int col, bool[,] visited)
-    {
-        if (index == word.Length) return true;
-        if (row < 0 || col < 0 || row >= board.GetLength(0) || col >= board.GetLength(1) || visited[row, col]) return false;
-
-        visited[row, col] = true;
-        board[row, col] = word[index];
-
-        List<(int, int)> directions = new List<(int, int)> { (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1) };
-        foreach (var (dr, dc) in directions)
-        {
-            if (PlaceWordFromPosition(board, word, index + 1, row + dr, col + dc, visited))
-            {
-                return true;
-            }
-        }
-
-        visited[row, col] = false;
-        board[row, col] = '\0';
-        return false;
-    }
-
-    void FillBoard(char[,] board)
-    {
-        for (int i = 0; i < board.GetLength(0); i++)
-        {
-            for (int j = 0; j < board.GetLength(1); j++)
-            {
-                if (board[i, j] == '\0')
-                    board[i, j] = (char)('A' + Random.Range(0,26));
-            }
+            BlockStatusChange(false);
+            SelectBlock();
         }
     }
 
-    void PrintBoard(char[,] board)
+    public void OnPointerUp(PointerEventData eventData)
     {
-        string boardString = "";
-        for (int i = 0; i < board.GetLength(0); i++)
+        if (eventData.button == PointerEventData.InputButton.Left)
         {
-            for (int j = 0; j < board.GetLength(1); j++)
+            BlockStatusChange(true);
+            _isClicked = false;
+            foreach (var block in _selected)
             {
-                boardString += board[i, j] + " ";
+                block.color = Color.white;
+                block.gameObject.GetComponent<Blocks>().Release();
             }
-            boardString += "\n";
+
+            _selected = new List<Image>();
         }
-        Debug.Log(boardString);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (_isClicked && !_isSelected)
+        {
+            SelectBlock();
+        }
+    }
+
+    private void SelectBlock()
+    {
+        blockImageComponent.color = Color.green;
+        _selected.Add(blockImageComponent);
+        word += character.ToString();
+        _isSelected = true;
+    }
+
+    public void Release()
+    {
+        _isSelected = false;
+    }
+
+    private static void BlockStatusChange(bool release)
+    {
+        if (release)
+            _isClicked = false;
+        else
+            _isClicked = true;
     }
 }
