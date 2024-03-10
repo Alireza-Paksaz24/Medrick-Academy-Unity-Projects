29b34383c119120ce0aaab1dcce3a751ed148e21
Alireza Paksasz <a.paksaz@outlook.com>
2024-03-11 00:08:51 +0330

    Init Setting Panel

---
+++ Alpha betty/Assets/Scripts/Setting.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setting : MonoBehaviour
+{
+    [SerializeField] private GameObject _canvas;
+    [SerializeField] private GameObject _settingCanvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OpenSettingPanel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OpenSettingPanel()
+    {
+        _canvas.SetActive(false);
+        _settingCanvas.SetActive(true);
+    }
+}
