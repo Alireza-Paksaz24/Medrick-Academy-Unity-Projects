196162afe3607377e9d45e5dc304cdbf579ecc7a
Alireza Paksasz <a.paksaz@outlook.com>
2024-03-11 02:02:49 +0330

    add animation to buttons

--- Alpha betty/Assets/Scripts/ButtonControl.cs
+++ Alpha betty/Assets/Scripts/ButtonControl.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ButtonControl : MonoBehaviour
@@ -12,12 +14,26 @@ public class ButtonControl : MonoBehaviour
     [SerializeField] private Button _settingButton;
 
     [SerializeField] private Button _refreshButton;
-    
-    // Start is called before the first frame update
     void Start()
     {
         _settingButton.onClick.AddListener(OpenSettingPanel);
         _refreshButton.onClick.AddListener(RefreshBoard);
+
+        // Create a new EventTrigger component if not already attached to the _refreshButton
+        EventTrigger trigger = _refreshButton.gameObject.GetComponent<EventTrigger>() ?? _refreshButton.gameObject.AddComponent<EventTrigger>();
+
+        // Create a new entry for the pointer enter event
+        EventTrigger.Entry pointerEnterEntry = new EventTrigger.Entry();
+        pointerEnterEntry.eventID = EventTriggerType.PointerEnter; // Specify the type of event
+
+        // Add a callback to the entry
+        pointerEnterEntry.callback.AddListener((data) => { PointerEnterRefresh((PointerEventData)data); });
+
+        // Add the entry to the triggers list
+        trigger.triggers.Add(pointerEnterEntry);
+        
+        _settingButton.transform.DORotate(new Vector3(0, 0, 360), 10, RotateMode.FastBeyond360)
+            .SetEase(Ease.Linear).SetLoops(-1, LoopType.Restart);
     }
 
     private void OpenSettingPanel()
@@ -31,4 +47,13 @@ public class ButtonControl : MonoBehaviour
     {
         GameObject.Find("Block_Panel").GetComponent<BlcoksManager>().Shuffle2DArray();
     }
+
+// Modified PointerEnterRefresh method to accept PointerEventData
+    private void PointerEnterRefresh(PointerEventData eventData)
+    {
+        _refreshButton.transform.DOKill();
+        _refreshButton.transform.DORotate(new Vector3(0, 0, 180), 1)
+            .SetEase(Ease.Linear);
+    }
+
 }
